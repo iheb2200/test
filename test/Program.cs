f48e8ccf93632c976f6b7621a519b2ec27005f77
Tec <Tec@SKAB1-PC2>
2022-11-30 08:37:28 +0100

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,5 @@
+﻿Kat meow = new("Grå");
+
+Console.WriteLine(meow.Farve);
+meow.SkiftFarve();
+Console.WriteLine(meow.Farve);
---
+++ test/kode/kat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.code;
+
+internal class Kat
+{
+    public string? Farve { get; set; }
+
+    public Kat(string farve)
+    {
+        Farve = farve;
+    }
+
+    public void SkiftFarve()
+    {
+        Farve = "Gul";
+    }
+}
